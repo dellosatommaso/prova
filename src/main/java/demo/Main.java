aab7a7ce45cc5baebab00de439c4a1db85733a59
lai2-24 <lai2-24@itismeucci.com>
2024-09-23 10:35:29 +0200

    contatore 100 e 200

--- src/main/java/demo/Main.java
+++ src/main/java/demo/Main.java
@@ -2,6 +2,10 @@ package demo;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("ciao mondo");
+        
+        MioThread t1 = new MioThread(100);
+        MioThread t2 = new MioThread(200);
+        t1.start();
+        t2.start();
     }
 }
